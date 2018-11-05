a0eddf56559ea139923fe48706e7c6322cf621b9
Kaden Dippe <kadendippe@berkeley.edu>
2018-11-04 21:43:03 -0800

    spotify authentication works: now using spotify-web-api-node

--- src/auth.js
+++ src/auth.js
@@ -2,44 +2,26 @@ var key = require('./key.js');
 const express = require('express');
 const request = require('request');
 const app = express();
-const SpotifyStrategy = require('passport-spotify').Strategy;
+const port = process.env.PORT || 8080;
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: key.client_id,
+  clientSecret: key.client_secret,
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
 
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: key.client_key,
-      clientSecret: key.client_secret,
-      callbackURL: 'http://localhost:8888/callback'
-    },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      process.nextTick(function() {
-          console.log('Profile': profile);
-          User.findOrCreate({
-              where: {
-                  SpotifyId: profile.id
-              },
-              defaults: {
-                  name: profile.displayName,
-                  SpotifyId: profile.id,
-                  accessToken: accessToken,
-                  proPic: profile.photos[0],
-                  refreshToken: refreshToken
-              }
-          }).spread(function (user) {
-              console.log('Making user: ', user);
-              done(null, user);
-          }).catch(done);
-      });
-    }
-  )
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  }
 );
-
-
