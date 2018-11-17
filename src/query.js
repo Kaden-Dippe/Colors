5c6c90ed8a64c70393abef5c5dadfcb643d7f85c
Kaden Dippe <kadendippe@berkeley.edu>
2018-11-16 16:21:33 -0800

    can get all songs for a given artist

--- src/query.js
+++ src/query.js
@@ -17,19 +17,43 @@ var SpotifyWebApi = require('spotify-web-api-node');
  *
  */
 
-/*** The functions that will be called by app.js ***/
+/** THESE ARE FUNCTIONS THAT WILL BE CALLED BY APP.JS*/
 
 
-/*** Search for an artist by name ***/
+/**
+* Search for an artist by name 
+*
+* @param {String} name Artists name which was typed into search bar
+* @return {Dictionary} nameToId Dictionary containing results of search. Key: name, Value: SpotifyId
+*/
+async function searchByName(name) {
+	var spotifyApi = await spotify_auth();
+	var nameToId = await searchForArtist(name, spotifyApi);
+	return nameToId;
+}
 
-/*** Once user verifies artist, search get songs based on that artists spotify Id
+//
 
+/** 
+* 
+* @param {String} id The Spotify id of the Artist
+* 
+*/
+async function getSongData(id) {
+  var spotifyApi = await spotify_auth();
+  var albums = await getAlbums(id, spotifyApi);
+  getSongs(albums, spotifyApi);
 
+}
 
+/**THESE ARE HELPER FUNCTIONS*/
 
-/**Returns spotiyApi object to make api calls***/
+/**
+* Get a spotify API token to make spotify API calls
+* 
+* @return {Spotify API Object} spotifyApi Spotify API object to make API calls
+*/
 async function spotify_auth() {
-  
   var spotifyApi = new SpotifyWebApi({
   clientId: key.client_id,
   clientSecret: key.client_secret,
@@ -37,7 +61,7 @@ async function spotify_auth() {
 
   try {
     var data = await spotifyApi.clientCredentialsGrant();
-    console.log(data.body['expires_in'])
+    //console.log(data.body['expires_in'])
   } catch(err) {
     console.log(err);
   }
@@ -49,6 +73,14 @@ async function spotify_auth() {
   return spotifyApi;
 }
 
+/** 
+* Create a Dictionary from searching for Artists. 
+* Dictionary of names => SpotifyIds.
+* 
+* @param {String} name Artists name to search for 
+* @param {Spotify API Object} spotifyApi Spotify API object to make API calls
+* @return {Dictionary} nameToId Dictionary containing results of search. Key: name, Value: SpotifyId
+*/
 async function searchForArtist(name, spotifyApi) {
   var nameToId = {};
   try { 
@@ -59,17 +91,91 @@ async function searchForArtist(name, spotifyApi) {
 
   data.body.artists.items.forEach(function(element) {
     nameToId[element.name] = element.id
-    //console.log(element.name);
     });
   return nameToId;
 }
 
-async function actions() {
-  spotifyApi = await spotify_auth();
-  var nameToId = await searchForArtist("Drake", spotifyApi);
-  //console.log(Object.keys(nameToId));
+
+async function getAlbums(id, spotifyApi) {
+  var albums = [];
+  try { 
+    var data = await spotifyApi.getArtistAlbums(id);
+  } catch(err) {
+    console.log(err);
+  }
+  
+  data.body.items.forEach(function(element) {
+    albums.push(element.id);
+  });
+
+  return albums;
+}
+
+//only getting first 100 songs
+async function getSongs(albums, spotifyApi) {
+  var songToFeatures = {};
+  var songs = []
+  var counter = 0
+  try { 
+    var data = await spotifyApi.getAlbums(albums);
+    data.body.albums.forEach(function(element) {
+      element.tracks.items.some(function(t) {
+        songs.push(t.id);
+        songToFeatures[t.id] = t.name;
+        counter += 1;
+        return counter >= 100;
+      });
+    });
+    console.log(counter);
+  } catch(err) {
+    console.log(err);
+  }
+
+  //assume songs isn't empty
+
+
+  console.log(songs.length);
+
+  /*
+  try {
+      var audioData = await spotifyApi.getAudioFeaturesForTracks(songs.slice(0,100));
+
+      audioData.body.audio_features.forEach(function(element) {
+        console.log(element);
+        var name = songToFeatures[song];
+      });
+    } catch(err) {
+      console.log(err);
+    }
+    counter += 1;
+  }
+
+    */
+
+
+
+  //newDict = {};
+
+  //try 
+  /*
+    songs2.forEach(async function(song) { 
+      try {
+        var audioData = await spotifyApi.getAudioFeaturesForTrack(song);
+        var name = songToFeatures[song];
+        console.log(name);
+        delete songToFeatures[song];
+        newDict[name] = audioData.body;
+        console.log(newDict);
+      } catch(err) {
+        console.log(err);
+      }
+    });
+
+  */
+  //console.log(newDict);
 }
 
 
-actions();
+//searchByName("21 Savage");
+getSongData('1URnnhqYAYcrqrcwql10ft');
 
